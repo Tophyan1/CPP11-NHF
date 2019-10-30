ecb5456c04f1192d00434a953010a2aeebf5eb88
Tophyan1 <Topyan99@gmail.com>
2019-10-30 19:21:41 +0100

    RETESTING EVERYTHING

--- test.cpp
+++ test.cpp
@@ -43,22 +43,22 @@ void test_all_Ctors() {
 }
 
 int main() {
-//    MyString x = "Hello ";
-//    MyString y = "World!";
-//    std::cout << x << y << std::endl;
-//    x[0] = 'C';
-//    MyString s = x + y;
-//    x += y;
-//    std::cout << s <<std::endl << x << std::endl;
-//    const MyString alma = "alma";
-//    std::cout << alma[0] << std::endl;
-//    MyString almafa = alma;
-//    (almafa += 'f') += 'a';
-//    std::cout << almafa << std::endl;
-//    MyString input;
-//    std::cin >> input;
-//    x = std::move(input);
-//    std::cout << x << std::endl;
+    MyString x = "Hello ";
+    MyString y = "World!";
+    std::cout << x << y << std::endl;
+    x[0] = 'C';
+    MyString s = x + y;
+    x += y;
+    std::cout << s <<std::endl << x << std::endl;
+    const MyString alma = "alma";
+    std::cout << alma[0] << std::endl;
+    MyString almafa = alma;
+    (almafa += 'f') += 'a';
+    std::cout << almafa << std::endl;
+    MyString input;
+    std::cin >> input;
+    x = std::move(input);
+    std::cout << x << std::endl;
 
     //New tests
 
